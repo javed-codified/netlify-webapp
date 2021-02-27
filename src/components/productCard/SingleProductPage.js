588bc808adee4c9f8a3530fc6786a2cafae2a791
deecoder <javiedahamedl@gmail.com>
2021-02-27 20:15:25 +0530

    Tabs single product page

--- src/components/productCard/SingleProductPage.js
+++ src/components/productCard/SingleProductPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import parse from 'html-react-parser'; 
+import parse from "html-react-parser";
+import { Row, Col, Tab, Tabs, Sonnet } from "react-bootstrap";
 
 import { getProductById } from "../../remote/woocommerce";
 
@@ -12,22 +13,39 @@ const SingleProductPage = (props) => {
     getProductById(props.match.params.id).then((response) => {
       console.log(response);
       setProduct(response.data);
-      setProductImg(response.data.images[0].src)
+      setProductImg(response.data.images[0].src);
     });
     setProduct_id(props.match.id);
     console.log(props.match.params.id);
-    console.log( "image", productImg);
-  },[]);
+    console.log("image", productImg);
+  }, []);
 
   return (
     <>
-      <h1> {product.name} </h1>
-       <h2>price =  {product.price} </h2>
-         {parse(product.price_html)}
-         {parse(product.short_description)}     
-         <img
-             src = {productImg}
-         />
+      <Row>
+        <Col md={6}>
+          <img src={productImg} />
+        </Col>
+        <Col md={6}>
+          <h1> {product.name} </h1>
+          <p>Sku:{product.sku}</p>
+          <h2>price = {product.price} </h2>
+          {parse(String(product.price_html))}
+          {parse(String(product.short_description))}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+            <Tab eventKey="home" title="Description">
+              <Row> {parse(String(product.description))} </Row>
+            </Tab>
+            <Tab eventKey="profile" title="Additional Information">
+              {parse(String(product.short_description))}
+            </Tab>
+          </Tabs>
+        </Col>
+      </Row>
     </>
   );
 };
