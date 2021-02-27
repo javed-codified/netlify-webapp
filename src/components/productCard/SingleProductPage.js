408a5c503f190b53490ad071e3800bbf5f54e77b
bedcodified <bed@codifiedweb.com>
2021-02-27 20:47:34 +0530

    update

--- src/components/productCard/SingleProductPage.js
+++ src/components/productCard/SingleProductPage.js
@@ -1,33 +1,31 @@
 import React, { useState, useEffect } from "react";
-import parse from 'html-react-parser'; 
+import parse from "html-react-parser";
 
 import { getProductById } from "../../remote/woocommerce";
 
 const SingleProductPage = (props) => {
   const [product, setProduct] = useState([]);
   const [product_id, setProduct_id] = useState();
-  const [productImg, setProductImg] = useState();
+  const [productImg, setProductImg] = useState("");
 
   useEffect(() => {
     getProductById(props.match.params.id).then((response) => {
       console.log(response);
       setProduct(response.data);
-      setProductImg(response.data.images[0].src)
+      setProductImg(response.data.images[0].src);
     });
     setProduct_id(props.match.id);
     console.log(props.match.params.id);
-    console.log( "image", productImg);
-  },[]);
+    console.log("image", productImg);
+  }, []);
 
   return (
     <>
       <h1> {product.name} </h1>
-       <h2>price =  {product.price} </h2>
-         {parse(product.price_html)}
-         {parse(product.short_description)}     
-         <img
-             src = {productImg}
-         />
+      <h2>price = {product.price} </h2>
+      {parse(String(product.price_html))}
+      {parse(String(product.short_description))}
+      <img src={productImg} />
     </>
   );
 };
